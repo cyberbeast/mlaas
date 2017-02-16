74fea1a557665474f5d02d5866943afe06909b04
cyberbeast <sandeshgade@gmail.com>
2017-02-16 15:32:52 -0500

    intermediary for pull

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { ModelClass } from '../models/model-class';
+
+const DEMO_MODELS: ModelClass[] = [
+  { _id: "OISAJFKALJS",
+    name: "Water Heater",
+    type: "Linear Regression",
+    parameters: "a=0.05",
+    train_status: "Trained",
+    deploy_status: "OFFLINE",
+    test_accuracy: "88%",
+    created_at: 2017,
+    updated_at: 2017},
+    { _id: "OISAJFKALSD",
+    name: "IoT Swarm",
+    type: "SVM",
+    parameters: "a=0.05",
+    train_status: "Trained",
+    deploy_status: "OFFLINE",
+    test_accuracy: "88%",
+    created_at: 2017,
+    updated_at: 2017},
+    { _id: "OISAJFKALFD",
+    name: "Thermostat Model",
+    type: "Linear Regression",
+    parameters: "a=0.05",
+    train_status: "Trained",
+    deploy_status: "OFFLINE",
+    test_accuracy: "88%",
+    created_at: 2017,
+    updated_at: 2017},
+]
 
 @Component({
   selector: 'app-dashboard',
@@ -6,6 +37,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  models = DEMO_MODELS;
 
   constructor() { }
 
