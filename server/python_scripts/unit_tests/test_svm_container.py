91f789fa9139d4e7deaab044bb43425a9c90481c
Spider101 <banerjee.abhimanyu3@gmail.com>
2017-02-23 21:14:50 -0500

    add code for first unit test

--- server/python_scripts/unit_tests/test_svm_container.py
+++ server/python_scripts/unit_tests/test_svm_container.py
@@ -6,5 +6,14 @@ from model_containers import svm_container
 
 class TestSVMContainer(unittest.TestCase):
 
-    def test_train(self):
-        pass
+    dataset_size = 10
+
+    def test_train_empty_return(self):
+        x = np.random.uniform(0, 6, (self.dataset_size, 4)).astype(np.float32)
+        y = np.random.choice([0,1], (self.dataset_size, 4)).astype(np.float32)
+        trainset = {"features" : x, "labels" : y} 
+        metrics = svm_container.SVMContainer().train(trainset)
+        self.assertFalse(len(metrics), 0)
+
+if __name__ == "__main__":
+    unittest.main()
