2d8a11504ccae174d18730bc529ebc0a67bd1240
Spider101 <banerjee.abhimanyu3@gmail.com>
2017-03-04 02:31:21 -0500

    add data container related scripts

---
+++ server/python_scripts/data_container/data_loader.py
@@ -0,0 +1,28 @@
+###############################################################################
+# Author: Abhimanyu Banerjee
+# Project: Machine Learning as a Service
+# Date Created: 3/3/2017
+#
+# File Description: This script implements the data container template for the 
+# purpose of loading both predefined and user defined datasets.
+###############################################################################
+
+from __future__ import print_function
+from data_container import DataContainer
+from utils.gen_utils import load_pkl, save_pkl
+from config import data_path
+from os.path import exists, path
+
+class DataLoader(DataContainer):
+
+    '''loads data from user's directory. Params: [], Returns: '''
+    def load_user_data(self, data_path):
+        #TODO: check for path name corruption
+        return load_pkl(data_path)
+
+    '''save data in user's directory. Params: [], Returns: '''
+    def save_user_data(self, data):
+        #TODO: define template for path for data storage
+        assert exists(data_path), "\nData directory does not exist"
+        path = join(data_path, "data_samples")
+        save_pkl(data, path)
