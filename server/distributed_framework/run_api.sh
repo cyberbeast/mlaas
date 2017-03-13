481d8986404a45b7801f9ed6296e00be4e47ef6f
cyberbeast <sandeshgade@gmail.com>
2017-03-13 15:23:16 -0400

    Wiki

--- server/distributed_framework/run_api.sh
+++ server/distributed_framework/run_api.sh
@@ -5,3 +5,4 @@ su -m myuser -c "gunicorn api:__hug_wsgi__ -b 0.0.0.0:5000"
 su -m myuser -c "export PYTHONPATH=$PYTHONPATH:/api/tasks/"
 # su -m myuser -c "rabbitmq-server"
 
+
--- server/distributed_framework/run_celery.sh
+++ server/distributed_framework/run_celery.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd api/tasks
 su -m myuser -c "celery -A tasks worker --loglevel=info"
+
