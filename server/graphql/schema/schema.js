def7f05d37d7694b264e075b2fe0a53e5cb0b64f
Sandesh Gade <sandeshgade@gmail.com>
2017-04-17 14:32:38 -0400

    Supporting graphql type defs

--- server/graphql/schema/schema.js
+++ server/graphql/schema/schema.js
@@ -1,12 +1,15 @@
 import { makeExecutableSchema } from 'graphql-tools';
 import Resolvers from '../resolvers/resolvers';
 import initStatusSchema from './initstatusmodel_schema';
-
-// # supplier(name: String): Supplier
+import mlModel from './mlmodel_schema';
+import ModelLoss from './modelloss';
+import ModelParameters from './modelparameters';
+import ModelMetrics from './modelmetrics';
 
 const RootQuery = `
   type RootQuery {
     getInitStatus: initStatus
+    getUserModels: [mlModel]
   }
 `;
 
@@ -17,6 +20,14 @@ const SchemaDefinition = `
 `;
 
 export default makeExecutableSchema({
-  typeDefs: [SchemaDefinition, RootQuery, initStatusSchema],
+  typeDefs: [
+    SchemaDefinition,
+    RootQuery,
+    initStatusSchema,
+    mlModel,
+    ModelLoss,
+    ModelParameters,
+    ModelMetrics
+  ],
   resolvers: Resolvers
 });
